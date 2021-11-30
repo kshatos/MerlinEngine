b01d421809809cd7b2f1103f1b14b6ff3a98b39c
Keagan Shatos <kshatos@live.com>
2021-11-29 23:05:56 -0800

    Added window event classes

--- Merlin/Merlin/Core/app_events.hpp
+++ Merlin/Merlin/Core/app_events.hpp
@@ -1,13 +1,16 @@
 #ifndef APP_EVENTS_HPP
 #define APP_EVENTS_HPP
 #include <string>
+#include <sstream>
 
 
 namespace Merlin
 {
-    enum AppEventType
+    enum class AppEventType
     {
-        None = 0
+        None = 0,
+        WindowClosed,
+        WindowResized
     };
 
     class AppEvent
@@ -18,6 +21,34 @@ namespace Merlin
         virtual std::string ToString() = 0;
     protected:
         bool was_handled = false;
+    };
+
+    // WINDOW EVENTS
+    class WindowClosedEvent : public AppEvent
+    {
+    public:
+        static AppEventType GetStaticType() { return AppEventType::WindowClosed; }
+        inline AppEventType GetType() override { return AppEventType::WindowClosed; };
+        std::string ToString() override { return "WindowClosedEvent"; }
+    };
+
+    class WindowResizedEvent : public AppEvent
+    {
+        unsigned int new_width;
+        unsigned int new_height;
+    public:
+        WindowResizedEvent(unsigned int width, unsigned int height)
+            : new_width(width), new_height(height) {}
+        static AppEventType GetStaticType() { return AppEventType::WindowResized; }
+        inline AppEventType GetType() override { return AppEventType::WindowResized; };
+        inline unsigned int GetWidth() { return new_width; }
+        inline unsigned int GetHeight() { return new_height; }
+        std::string ToString() override
+        {
+            std::ostringstream oss;
+            oss << "WindowResizedEvent (" << new_width << ", " << new_height << ")";
+            return oss.str();
+        }
     };
 
 }
