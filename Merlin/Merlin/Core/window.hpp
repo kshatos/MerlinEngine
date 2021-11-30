15d784f9180a04a2920f86dadbda3fc15606117b
Keagan Shatos <kshatos@live.com>
2021-11-30 09:36:10 -0800

    Added window abstract base class

---
+++ Merlin/Merlin/Core/window.hpp
@@ -0,0 +1,33 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+#include <functional>
+#include "app_events.hpp"
+
+
+namespace Merlin
+{
+    struct WindowProperties
+    {
+        std::string title;
+        unsigned int width;
+        unsigned int height;
+
+        WindowProperties(
+            std::string _title, int _width, int _height) :
+            title(_title), width(_width), height(_height)
+        {
+        }
+    };
+
+    class Window
+    {
+        using EventCallbackFunction = std::function<void(AppEvent&)>;
+
+        virtual ~Window() {}
+        virtual unsigned int GetWidth() = 0;
+        virtual unsigned int GetHeight() = 0;
+        virtual void SetEventCallback(const EventCallbackFunction& callback) = 0;
+        virtual void OnUpdate() = 0;
+    };
+}
+#endif
