14eb18120bfa4a642e748fca03de9d95350d02b5
Keagan Shatos <kshatos@live.com>
2021-12-01 10:59:21 -0800

    Added shader abstract base class with minimal API.

---
+++ Merlin/Merlin/Render/shader.cpp
@@ -0,0 +1,10 @@
+#include "Merlin/Render/shader.hpp"
+
+
+namespace Merlin
+{
+    Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
+    {
+        return nullptr;
+    }
+}
