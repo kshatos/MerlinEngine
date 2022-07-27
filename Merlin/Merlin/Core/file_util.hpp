efbcd24ef6b68caa6d522593f57014acfc936305
Keagan Shatos <kshatos@live.com>
2022-07-27 09:58:50 -0700

    Added loading funtion. Issue with uv coords.

--- Merlin/Merlin/Core/file_util.hpp
+++ Merlin/Merlin/Core/file_util.hpp
@@ -1,11 +1,16 @@
 #ifndef FILE_UTIL_HPP
 #define FILE_UTIL_HPP
+#include "Merlin/Render/mesh.hpp"
 #include <string>
+#include <memory>
 
 
 namespace Merlin
 {
     class Texture2DData;
     Texture2DData load_texture(std::string filepath);
+
+    struct Vertex_XNUV;
+    std::shared_ptr<Mesh<Vertex_XNUV>> load_mesh(std::string filepath);
 }
 #endif
