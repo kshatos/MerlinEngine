0038904e76b107fb9efd32e6bdd17b4b2fe1b830
Keagan Shatos <kshatos@live.com>
2022-08-25 15:35:08 -0700

    Formatted demo code.

--- VulkanTest/source/main.cpp
+++ VulkanTest/source/main.cpp
@@ -1,14 +1,15 @@
-#include <sstream>
-#include <glm/gtc/random.hpp>
-#include <glm/glm.hpp>
 #include <imgui.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/random.hpp>
+#include <sstream>
+
 #include "Merlin/Core/core.hpp"
+#include "Merlin/Core/file_util.hpp"
 #include "Merlin/Render/render.hpp"
 #include "Merlin/Scene/scene.hpp"
-#include "Merlin/Core/file_util.hpp"
 
 using namespace Merlin;
-
 
 class ImGUITestLayer : public Layer
 {
@@ -19,38 +20,27 @@ public:
     virtual void OnAttach() override
     {
         std::vector<float> vertices{
-            1.0, 2.0, 3.0,
-            4.0, 5.0, 6.0,
-            7.0, 8.0, 9.0
-        };
-        std::vector<uint32_t> indices{
-            0, 1, 2
-        };
-        BufferLayout layout{
-            {ElementDataType::Float3, "Pos"}
-        };
+            1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
+        std::vector<uint32_t> indices{0, 1, 2};
+        BufferLayout layout{{ElementDataType::Float3, "Pos"}};
 
-        m_meshBuffer = Renderer::CreateMeshBuffer(
-            vertices.data(),
-            vertices.size() * sizeof(float),
-            indices.data(),
-            indices.size(),
-            layout);
+        m_meshBuffer =
+            Renderer::CreateMeshBuffer(vertices.data(),
+                                       vertices.size() * sizeof(float),
+                                       indices.data(),
+                                       indices.size(),
+                                       layout);
 
         m_texture = Renderer::CreateTexture2D(
             LoadTexture(".\\Assets\\Textures\\debug.jpg"));
     };
 
-    virtual void OnDetatch() override {};
+    virtual void OnDetatch() override{};
 
-    virtual void OnUpdate(float time_step) override
-    {
-        ImGui::ShowDemoWindow();
-    }
+    virtual void OnUpdate(float time_step) override { ImGui::ShowDemoWindow(); }
 
-    virtual void HandleEvent(AppEvent& app_event) override {};
+    virtual void HandleEvent(AppEvent& app_event) override{};
 };
-
 
 class VulkanTestApp : public Application
 {
@@ -61,16 +51,14 @@ public:
     }
 };
 
-
 void main()
 {
-    ApplicationInfo appInfo
-    {
+    ApplicationInfo app_info{
         "VulkanTest",
         RenderBackend::Vulkan,
         800,
-        800
+        800,
     };
-    VulkanTestApp app(appInfo);
+    VulkanTestApp app(app_info);
     app.Run();
 }
