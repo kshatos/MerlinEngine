20ca7234aa556621a553b87e801ad41dd0c03c59
Keagan Shatos <kshatos@live.com>
2022-09-09 23:27:37 -0700

    Added directory iteration/presentation and directory traversal to editor panel.

--- MerlinEditor/asset_explorer_panel.cpp
+++ MerlinEditor/asset_explorer_panel.cpp
@@ -7,6 +7,42 @@ namespace MerlinEditor
     void AssetExplorerPanel::DrawPanel()
     {
         ImGui::Begin(m_name.c_str());
+
+        bool go_back = ImGui::Button("BACK");
+        ImGui::SameLine();
+        auto current_path_string = m_current_path.string();
+        ImGui::Text("%s", current_path_string.c_str());
+
+        ImGui::Separator();
+
+        float cellSize = 200.0f;
+        float panelWidth = ImGui::GetContentRegionAvail().x;
+        int columnCount = (int)(panelWidth / cellSize);
+        if (columnCount < 1) columnCount = 1;
+
+        ImGui::Columns(columnCount, 0, false);
+
+        std::filesystem::directory_iterator directory_iterator(
+            m_current_path);
+        for (const auto& entry : directory_iterator)
+        {
+            const auto& path = entry.path();
+            std::string filename_string = path.filename().string();
+            if (ImGui::Button(filename_string.c_str()))
+            {
+                if (entry.is_directory())
+                {
+                    m_current_path = entry.path();
+                }
+            }
+
+            ImGui::NextColumn();
+        }
+
+        if (go_back)
+        {
+            m_current_path = m_current_path.parent_path();
+        }
         ImGui::End();
     }
 }  // namespace MerlinEditor
--- MerlinEditor/asset_explorer_panel.hpp
+++ MerlinEditor/asset_explorer_panel.hpp
@@ -1,5 +1,6 @@
 #ifndef ASSET_EXPLORER_PANEL_HPP
 #define ASSET_EXPLORER_PANEL_HPP
+#include <filesystem>
 #include <string>
 
 #include "editor_panel.hpp"
@@ -9,6 +10,8 @@ namespace MerlinEditor
     class AssetExplorerPanel : public EditorPanel
     {
         std::string m_name = "Asset Explorer";
+        const std::string m_asset_path = "Assets";
+        std::filesystem::path m_current_path = std::filesystem::current_path();
 
     public:
         inline std::string GetPanelName() { return m_name; }
